89ec1fd518d103914f722947445fbfd68c905f70
Raymond Ong <remondo@gmail.com>
2019-12-19 13:26:57 +0800

    jsPDF Practice - Recharts pie and bar - dc.js imagemap - charts using html2canvas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,96 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOMServer from "react-dom/server";
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import canvgX from 'canvg';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+var doc = new jsPDF();
+// debugger
+// html2canvas(document.getElementById('root'), {
+//     // this does not work
+//     onrendered: function(canvas) {
+//         // debugger
+//         var img = canvas.toDataURL(); //image data of canvas
+//         var doc = new jsPDF();
+//         doc.addImage(img, 10, 10);
+//         //doc.save('test.pdf');
+//     }
+// })
+
+// html2canvas(document.getElementById('mypie1'), {
+//         scale: 2
+//     }
+// ).then(function(canvas) {
+//     //debugger
+//     var img = canvas.toDataURL(); //image data of canvas
+//     var doc = new jsPDF();
+//     doc.addImage(img, 10, 10);
+//     //doc.save('test.pdf');    
+// });
+// doc.fromHTML(ReactDOMServer.renderToStaticMarkup(<App/>));
+
+//doc.save("myDocument.pdf");
+
+const covertToMm = val => val / 3.7795275591;
+  
+const onSave = () => {
+    console.log('start save');
+    let p1 = html2canvas(document.getElementById('mypie1'));
+    let p2 = html2canvas(document.getElementById('imagemapwrapper'));
+    let p3 = html2canvas(document.getElementById('myPieChartId'));    
+    //let p4 = html2canvas(document.getElementById('myTableId'));
+    let p5 = html2canvas(document.getElementById('mybar1'));
+
+    Promise.all([p1,p2]).then(function(allPromises) {
+        let y = 10;
+        var pageHeight= doc.internal.pageSize.height;
+        console.log('All promises done', allPromises);
+        //addSvg(doc);
+        allPromises.forEach(canvas => {
+            
+            if (y >= pageHeight)
+            {
+                doc.addPage();
+                y = 0 // Restart height position
+            }
+
+            var img = canvas.toDataURL();
+            var height = covertToMm(canvas.height)
+            doc.addImage(img, 10, y);            
+            y += height;
+        })
+
+        
+        doc.save('test.pdf');
+    });
+
+
+    console.log('end save');
+}
+
+const addSvg = (doc) => {
+    debugger
+    let elem = document.getElementById('myImageMap');
+    // var svgAsText = new XMLSerializer().serializeToString(elem);
+    // doc.addSVG(svgAsText, 10, 10, 100, 100)
+    
+    
+    var svg = elem.innerHTML;
+    svg = svg.replace(/\r?\n|\r/g, '').trim();
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    canvgX.fromString(context, svg);
+    debugger
+
+    var imgData = canvas.toDataURL('image/png');
+    doc.addImage(imgData, 'PNG', 0, 0, 100, 100);
+}
+
+
+
+ReactDOM.render(<App onSave={onSave}/>, document.getElementById('root'));
---
+++ src/pieChart.js
@@ -0,0 +1,62 @@
+import React, { PureComponent } from 'react';
+import {
+  PieChart, Pie, Sector, Cell, ResponsiveContainer, Legend
+} from 'recharts';
+
+const data = [
+  { name: 'Good', value: 400 },
+  { name: 'Bad', value: 300 },
+  { name: 'Fair', value: 300 },
+  { name: 'Uncertain', value: 200 },
+];
+
+const COLORS = ['green', 'red', 'gold', 'gray'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx, cy, midAngle, innerRadius, outerRadius, percent, index, name
+}) => {
+   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${name} - ${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+let renderLabel = function(entry) {
+    return `${entry.name} - ${(entry.percent*100).toFixed(1)}%`;
+}
+
+export default class PieSample extends PureComponent {
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/c9pL8k61/';
+
+  render() {
+    return (
+    <ResponsiveContainer  width="100%" >
+      <PieChart margin={{top: 25, right: 30, left: 20, bottom: 25}}
+          >
+        <Pie
+          data={data}
+          labelLine={true}
+        //   label={renderCustomizedLabel}
+        label
+        //label={renderLabel}
+        //   outerRadius={100}
+          fill="#8884d8"
+          dataKey="value"
+          isAnimationActive={false}
+        >
+          {
+            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+          }
+        </Pie>
+        <Legend verticalAlign="top" height={36}/>
+      </PieChart>
+    </ResponsiveContainer>
+    );
+  }
+}
